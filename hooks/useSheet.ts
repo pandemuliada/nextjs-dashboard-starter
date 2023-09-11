d8710ded0e71ea9136e206aa645abf51f04b1ace
pandemuliada <pandemuliada@gmail.com>
2023-09-11 12:34:13 +0800

    remove unused file

--- hooks/useSheet.ts
+++
@@ -1,9 +0,0 @@
-import { OverlayTriggerProps, useOverlayTriggerState } from "react-stately";
-
-const useSheet = (props: OverlayTriggerProps = {}) => {
-  const state = useOverlayTriggerState(props);
-
-  return { state };
-};
-
-export default useSheet;
